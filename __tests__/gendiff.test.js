121b60cb167ceddbf6dfcf374bc12b72b62d5d18
Vadim <v_a_d@inbox.ru>
2019-02-12 23:30:08 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,14 +4,8 @@ import gendiff from '../src/bin/gendiff';
 
 const firstPath = './__tests__/__fixtures__/before.json';
 const secondPath = './__tests__/__fixtures__/after.json';
-const absoluteFirstPath = '/home/vadim/Desktop/gendiff/__tests__/__fixtures__/before.json';
-const absoluteSecondPath = '/home/vadim/Desktop/gendiff/__tests__/__fixtures__/after.json';
 const checkString = fs.readFileSync('./__tests__/__fixtures__/result', 'utf-8');
 
 test('test gendiff', () => {
   expect(gendiff(firstPath, secondPath)).toBe(checkString);
-});
-
-test('test gendiff with absolute paths', () => {
-  expect(gendiff(absoluteFirstPath, absoluteSecondPath)).toBe(checkString);
 });
